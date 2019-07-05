a7aebf4ac21b63b19f062bf7450ad2b713096ad0
foxjazz <fox21@foxjazz.net>
2019-07-05 13:29:02 -0600

    added run to script

--- doBuildPushRun.sh
+++ doBuildPushRun.sh
@@ -2,3 +2,7 @@
  dotnet publish -c Release --framework netcoreapp3.0
  rm EveAlert.zip
 "c:\program files\7-zip\7z.exe" a -r \EveAlert.zip .\bin\Release\netcoreapp3.0\publish
+git commit -m "updated"
+git push origin master
+dotnet run .\bin\Release\netcoreapp3.0\EveIntelReader.dll
+
